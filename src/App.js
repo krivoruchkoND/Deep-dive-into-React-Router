86eb8e570b7e6ceb35b087cdf0cd73ba1b7e560a
Nikita_Krivoruchko <knd@liis.su>
2020-10-25 19:51:03 +0300

    dynamic routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
 import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
+const users = [
+  {
+    name: `Param`,
+  },
+  {
+    name: `Vennila`,
+  },
+  {
+    name: `Afrin`,
+  },
+];
+
 const IndexPage = () => {
   return (
     <h3>Home Page</h3>
+  );
+};
+
+const UsersPage = () => {
+  return (
+    <>
+      <h3>Users Page</h3>
+      {users.map((user, index) => (
+        <h5 key={index}>
+          <Link to={`/user/${index + 1}`}>{user.name}'s Page</Link>
+        </h5>
+      ))}
+    </>
+  );
+};
+
+const UserPage = ({ match, location }) => {
+  const { params: { userId } } = match;
+
+  return (
+    <>
+      <p>
+        <strong>User ID: </strong>
+        {userId}
+      </p>
+      <p>
+        <strong>User Name: </strong>
+        {users[userId - 1].name}
+      </p>
+    </>
   );
 };
 
@@ -18,8 +60,11 @@ function App() {
     <section className="App">
       <Router>
         <Link to="/">Home</Link>
+        <Link to="/users">Users</Link>
         <Link to="/about">About</Link>
         <Route exact path="/" component={IndexPage} />
+        <Route exact path="/users" component={UsersPage} />
+        <Route exact path="/user/:userId" component={UserPage} />
         <Route exact path="/about" component={AboutPage} />
       </Router>
     </section>
